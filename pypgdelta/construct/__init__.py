306c44c04d81507ec9947b008f17570d3323094c
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-04 21:48:29 +0200

    Add constructor function

---
+++ pypgdelta/construct/__init__.py
@@ -0,0 +1,1 @@
+from ._construct import construct_configuration
---
+++ pypgdelta/construct/_construct.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+from typing import List, Dict
+
+
+def construct_configuration(statements: List[Dict]) -> Dict:
+    """Function for getting the configuration based on the input statements (as dicts)
+
+    :param List[Dict] statements: The statements upon which to base the configuration
+
+    :return: The configuration
+    :rtype: Dict
+    """
+
+    return OrderedDict()
