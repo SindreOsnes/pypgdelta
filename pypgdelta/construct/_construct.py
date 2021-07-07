42fa7b46f23ff81a79ec527f3d0398808a09e643
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-07 20:19:44 +0200

    Separate schema extraction into separate function

--- pypgdelta/construct/_construct.py
+++ pypgdelta/construct/_construct.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 from typing import List, Dict
 
+from ._schema import create_schema_baseline
+
 
 def construct_configuration(statements: List[Dict]) -> Dict:
     """Function for getting the configuration based on the input statements (as dicts)
@@ -15,12 +17,6 @@ def construct_configuration(statements: List[Dict]) -> Dict:
     for statement in statements:
         statements_list.extend(statement.get('stmts', []))
 
-    schema_statements = [statement for statement in statements_list if 'CreateSchemaStmt' in statement.get('stmt', {})]
-    configuration = OrderedDict(
-        [
-            (statement['stmt']['CreateSchemaStmt']['schemaname'], OrderedDict())
-            for statement in schema_statements
-        ]
-    )
+    baseline = create_schema_baseline(statements_list)
 
-    return configuration
+    return baseline
---
+++ pypgdelta/construct/_schema.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+from typing import Iterable, Dict
+
+
+def create_schema_baseline(statements: Iterable[Dict]) -> Dict:
+    """Function for getting the baseline schema configuration based on create schema statements
+
+    :param List[Dict] statements: The statements upon which to base the configuration
+
+    :return: The configuration
+    :rtype: Dict
+    """
+
+    schema_statements = [statement for statement in statements if 'CreateSchemaStmt' in statement.get('stmt', {})]
+    configuration = OrderedDict(
+        [
+            (statement['stmt']['CreateSchemaStmt']['schemaname'], OrderedDict())
+            for statement in schema_statements
+        ]
+    )
+
+    return configuration
