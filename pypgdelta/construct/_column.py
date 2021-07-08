b85d66f9515fe44561a5d9cf694863b7f5a4ce5c
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-08 22:24:00 +0200

    Bugfix: Set not null

--- pypgdelta/construct/_column.py
+++ pypgdelta/construct/_column.py
@@ -26,7 +26,7 @@ def create_column_config(column_def: Dict) -> Dict:
     column_config['nullable'] = True
     for constraint in column_def.get('constraints', []):
         if constraint.get('Constraint', {}).get('contype', None) == "CONSTR_NOTNULL":
-            column_config['nullable'] = True
+            column_config['nullable'] = False
 
     if 'data_type' not in column_config:
         raise TypeError(f'Unable to handle column type {col_type}')
