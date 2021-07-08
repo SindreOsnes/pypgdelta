6aab9a0bf8c74380e57c1a6d4d84a45d9c5156d0
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-08 23:14:06 +0200

    Add alter table function

--- pypgdelta/sql/statements/_table.py
+++ pypgdelta/sql/statements/_table.py
@@ -29,3 +29,36 @@ def create_table(schema_name: str, table_name: str, column_definitions: Dict) ->
     table_statement = f"CREATE TABLE {schema_name}.{table_name} (\n\t{column_statements}\n)"
 
     return table_statement
+
+
+def alter_table(schema_name: str, table_name: str,
+                new_column_definitions: Dict,
+                alter_column_definitions: Dict,
+                delete_column_definitions: Dict) -> str:
+    """Function for generating a create table statement
+
+    :param str schema_name: The name of the schema that the table belongs to
+    :param str table_name: The name of the table in question
+    :param Dict new_column_definitions: The column definitions to be added
+    :param Dict alter_column_definitions: The column definitions to be altered
+    :param Dict delete_column_definitions: The column definitions to be deleted
+
+    :return: The sql query
+    :rtype: str
+    """
+
+    # create the columns
+    new_column_statements = ',\n\t'.join(
+        [
+            create_column_statement(
+                name=name,
+                data_type=properties['data_type']
+            )
+            for name, properties in new_column_definitions.items()
+        ]
+    )
+
+    # Create the table statement based on the column
+    table_statement = f"ALTER TABLE {schema_name}.{table_name} \n{new_column_statements}"
+
+    return table_statement
