37de162d1775271763c46a658a34d6e070e415bf
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-08 23:44:43 +0200

    Add nullability to column statement

--- pypgdelta/sql/statements/_column.py
+++ pypgdelta/sql/statements/_column.py
@@ -1,8 +1,9 @@
-def create_column_statement(name: str, data_type: str, **kwargs) -> str:
+def create_column_statement(name: str, data_type: str, nullable: bool, **kwargs) -> str:
     """Function for generating column definitions
 
     :param str name: The name of the columns
     :param str data_type: The data type in question
+    :param bool nullable: Whether the column is nullable
     :param kwargs:
 
     :return: The column sql definition
@@ -10,5 +11,26 @@ def create_column_statement(name: str, data_type: str, **kwargs) -> str:
     """
 
     col_str = f"{name} {data_type}"
+    if not nullable:
+        col_str += ' NOT NULL'
+
+    return col_str
+
+
+def alter_column_statement(name: str, data_type: str, nullable: bool, **kwargs) -> str:
+    """Function for generating column definitions
+
+    :param str name: The name of the columns
+    :param str data_type: The data type in question
+    :param bool nullable: Whether the column is nullable
+    :param kwargs:
+
+    :return: The column sql definition
+    :rtype: str
+    """
+
+    col_str = f"{name} {data_type}"
+    if not nullable:
+        col_str += ' NOT NULL'
 
     return col_str
