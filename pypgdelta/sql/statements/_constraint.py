0060dd9d7126b5e206ec7bb13efea784c23f2750
Sindre Osnes <sindre.osnes@gmail.com>
2021-07-18 23:32:08 +0200

    Add constraint statement creation

---
+++ pypgdelta/sql/statements/_constraint.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+from typing import Dict
+
+
+def create_constraint_statements(schema_name: str,
+                                 table_name: str,
+                                 constraints: Dict,
+                                 **kwargs) -> Dict:
+    """Function for generating primary key definitions definitions
+
+    :param str schema_name: The name of the schema the table belongs to
+    :param str table_name: The name of the table
+    :param Dict constraints: The constraint definitions
+    :param kwargs:
+
+    :return: The constraint statements
+    :rtype: str
+    """
+
+    statements = OrderedDict(
+        [
+            ('drop', []),
+            ('create', [])
+        ]
+    )
+
+    if 'new_pk' in constraints:
+        statement = f"ALTER TABLE {schema_name}.{table_name} ADD CONSTRAINT {constraints['new_pk']['name']}"
+        statement += f" PRIMARY KEY({','.join(constraints['new_pk']['columns'])})"
+        statements['create'].append(statement)
+    if 'old_pk' in constraints:
+        statement = f"ALTER TABLE {schema_name}.{table_name} DROP CONSTRAINT {constraints['new_pk']['name']}"
+        statements['drop'].append(statement)
+
+    return statements
